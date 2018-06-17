c28f246c2e133a29ed9bdbc1fa09ab345db8143b
Man Ioan <man_ioan@live.com>
2018-06-17 21:25:34 +0300

    Add project files.

---
+++ SendMailAsp/SendMailOutlook.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SendMailOutlook.aspx.cs" Inherits="SendMailAsp.SendMailOutlook" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: xx-large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+            &nbsp;Send Email to : &nbsp;&nbsp;&nbsp;&nbsp;<asp:DropDownList ID="DropDownListEmail" runat="server" Width="309px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="ButtonSendMail" runat="server" OnClick="ButtonSendMail_Click" style="margin-left: 84px" Text="Send Mail" Width="289px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ SendMailAsp/SendMailOutlook.aspx.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using SendMailAsp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SendMailAsp
+{
+    public partial class SendMailOutlook : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DropDownListEmail.Items.Add("someone@live.com");
+                DropDownListEmail.Items.Add("someone@yahoo.com");
+            }
+        }
+
+        protected void ButtonSendMail_Click(object sender, EventArgs e)
+        {
+            SendEmailViaOutLook();
+            DropDownListEmail.Text = "";
+        }
+
+        public void SendEmailViaOutLook()
+        {
+            try
+            {
+                Application OutlookApplication = new Application();
+                MailItem message = (MailItem)OutlookApplication.CreateItem(OlItemType.olMailItem);
+                MailAddress toAddress = new MailAddress(DropDownListEmail.Text);
+                //MailAddress ccAddress = new MailAddress("ccmail@gmail.com");
+
+                message.To = toAddress.ToString();
+                //message.CC = ccAddress.ToString();
+                message.Subject = "Mail Subject";
+                message.Body = "Hello";
+                message.BodyFormat = OlBodyFormat.olFormatHTML;
+
+                //Send email
+                message.Send();
+                MessageBox.Show("Mesaj trimis cu succes!");
+
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+    }
+}
